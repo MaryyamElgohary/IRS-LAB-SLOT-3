278637d11f0140775707167bca6284fe136ab88b
0shek0 <sbakr558@gmail.com>
2025-10-21 14:33:18 +0300

    Add files via upload

---
+++ shereef bakr 221100940.py
@@ -0,0 +1,40 @@
+import numpy as np
+def cosine_similarity(user1, user2):
+    """calculate cosine similarity between two users"""
+    mask = (user1 !=0) &(user2 !=0)
+    
+    
+    if not np.any(mask):
+        return 0
+    
+    
+    u1 = user1[mask]
+    u2 = user2[mask]
+    
+    dot_producut = np.sum(u1 *u2)
+    
+    
+    mag1 = np.sqrt(np.sum(u1**2))
+    mag2 = np.sqrt(np.sum(u2**2))
+     
+    if mag1 ==0 or mag2 ==0 :
+        return 0
+    
+    return dot_producut / (mag1 * mag2)
+
+user_A =np.array([5,3,0,4,0,2])
+user_B =np.array([3,0,2,3,3,0])
+user_C =np.array([4,3,4,3,5,3])
+similarity_AB = cosine_similarity(user_A, user_B)
+similarity_AC = cosine_similarity(user_A, user_C)
+
+print (f"cosine similarity A-B : {similarity_AB:.3f}")
+print (f"cosine similarity A-C : {similarity_AC:.3f}")
+
+
+
+
+
+    
+    
+         
